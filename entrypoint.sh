7f34fb3a2a7be77829e3b013594fee0e5a1a0c15
sunnyqeen <sunnyqeen@gmail.com>
2024-03-15 13:48:37 +0100

    local feed lists first
    Signed-off-by: sunnyqeen <sunnyqeen@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,15 +40,15 @@ if [ -n "$PRIVATE_KEY" ]; then
 	CONFIG_SIGNED_PACKAGES="y"
 fi
 
+echo "src-link $FEEDNAME /feed/" > feeds.conf
+
 if [ -z "$NO_DEFAULT_FEEDS" ]; then
 	sed \
 		-e 's,https://git.openwrt.org/feed/,https://github.com/openwrt/,' \
 		-e 's,https://git.openwrt.org/openwrt/,https://github.com/openwrt/,' \
 		-e 's,https://git.openwrt.org/project/,https://github.com/openwrt/,' \
-		feeds.conf.default > feeds.conf
+		feeds.conf.default >> feeds.conf
 fi
-
-echo "src-link $FEEDNAME /feed/" >> feeds.conf
 
 ALL_CUSTOM_FEEDS="$FEEDNAME "
 #shellcheck disable=SC2153
